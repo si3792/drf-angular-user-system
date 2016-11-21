91e6838a6cd5f0230e2f2f0a7bb7eae855d94e36
si3792 <simo.t.iliev@gmail.com>
2016-11-21 20:33:17 +0200

    Added more tests

---
+++ client/app/directives/location-changer/location-changer.spec.js
@@ -0,0 +1,37 @@
+describe("cd-location-changer", function() {
+
+    var $location, $rootScope, $compile;
+    var scope;
+
+    function mockAuthService() {}
+
+    beforeEach(module('mainApp', function($provide) {
+
+        $provide.service('AuthService', mockAuthService);
+        // Turn off console logs
+        DEBUG = false;
+    }));
+
+    beforeEach(inject(function(_$location_, _$rootScope_, _$compile_) {
+        $location = _$location_;
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+
+    it("should change location when clicked", function() {
+
+        mockAuthService.prototype.isAuthenticated = function() {
+            return true;
+        };
+
+        var element = angular.element('<a cd-location-changer="/home"> Click </a>');
+        scope = $rootScope.$new();
+        var compiledElement = $compile(element)(scope);
+
+        expect($location.path()).toEqual('');
+        compiledElement.triggerHandler('click');
+        scope.$digest();
+        expect($location.path()).toEqual('/home');
+    });
+
+});
---
+++ client/app/directives/logout/logout.spec.js
@@ -0,0 +1,73 @@
+describe("cd-logout", function() {
+
+    var $location, $rootScope, $compile, $q;
+    var scope;
+
+    function mockAuthService() {}
+
+    function mockAlertModalService() {}
+
+    beforeEach(module('mainApp', function($provide) {
+
+        $provide.service('AuthService', mockAuthService);
+        $provide.service('AlertModalService', mockAlertModalService);
+
+        DEBUG = false; // Turn off console logs
+    }));
+
+    beforeEach(inject(function(_$location_, _$rootScope_, _$compile_, _$q_) {
+        $location = _$location_;
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+        $q = _$q_;
+    }));
+
+    it("should logout the user when clicked", function() {
+
+        mockAuthService.prototype.logout = function() {
+            var deferred = $q.defer();
+            deferred.resolve();
+            return deferred.promise;
+        }
+        spyOn(mockAuthService.prototype, "logout").and.callThrough();
+
+        var element = angular.element('<cd-logout> Click me to logout </cd-logout>');
+        scope = $rootScope.$new();
+        var compiledElement = $compile(element)(scope);
+
+        expect($location.path()).toEqual('');
+        compiledElement.triggerHandler('click');
+        scope.$digest();
+        expect($location.path()).toEqual('/login');
+        expect(mockAuthService.prototype.logout).toHaveBeenCalled();
+    });
+
+    it("should display warning when forceLogout was required to logout", function() {
+
+        mockAuthService.prototype.logout = function() {
+            var deferred = $q.defer();
+            deferred.reject();
+            return deferred.promise;
+        }
+        spyOn(mockAuthService.prototype, "logout").and.callThrough();
+
+        mockAlertModalService.prototype.alert = function() {
+            var deferred = $q.defer();
+            deferred.resolve();
+            return deferred.promise;
+        }
+        spyOn(mockAlertModalService.prototype, "alert").and.callThrough();
+
+        var element = angular.element('<cd-logout> Click me to logout </cd-logout>');
+        scope = $rootScope.$new();
+        var compiledElement = $compile(element)(scope);
+
+        expect($location.path()).toEqual('');
+        compiledElement.triggerHandler('click');
+        scope.$digest();
+        expect($location.path()).toEqual('/login');
+        expect(mockAuthService.prototype.logout).toHaveBeenCalled();
+        expect(mockAlertModalService.prototype.alert).toHaveBeenCalled();
+    });
+
+});
